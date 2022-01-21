c040022b21ee681f0a8646d42d50c1b8b27c5ed3
Joshua Adesanya <adesanyajoshua@ymail.com>
2022-01-21 03:21:22 +0100

    new changes

--- controllers/detail-controller.js
+++ controllers/detail-controller.js
@@ -4,27 +4,22 @@ const { v4: uuidv4 } = require("uuid");
 
 const detailService = new DetailService();
 
-
-exports.createDetail  = async (req, res) => {
+exports.createDetail = async (req, res) => {
   try {
-    const { social, number, } = req.body;
+    const { social, number } = req.body;
 
     const detail = await detailService.createDetail(req.body);
 
     const response = new Response(
       true,
       200,
-      "Question created successfully",
+      "Detail created successfully",
       detail
     );
     return res.status(response.code).json(response);
-
-    
   } catch (err) {
     console.log(err);
     const response = new Response(false, 500, "Server Error", err);
     return res.status(response.code).json(response);
   }
 };
-
-
