9b2da482de255e4d2af084c278b09604577cbf3f
Joshua Adesanya <adesanyajoshua@ymail.com>
2022-03-07 13:00:20 +0100

    new

--- config/config.js
+++ config/config.js
@@ -1,17 +1,17 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const path = require("path");
 
-if (!process.env.HOST){
+if (!process.env.HOST) {
   dotenv.config({
-    path: path.join(__dirname, "..", ".env")
-  })
+    path: path.join(__dirname, "..", ".env"),
+  });
 }
-
+console.log(process.env.PASS);
 module.exports = {
   host: process.env.HOST,
   username: process.env.POST_USER,
   password: process.env.PASS,
   port: process.env.DB_PORT,
   database: process.env.DB,
-  dialect: "postgres"
-}
+  dialect: "postgres",
+};
